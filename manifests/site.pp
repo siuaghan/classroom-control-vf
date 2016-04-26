d25a76bd5d75e4c0149d89fb34f3b5fcc4fda346
siuaghan <siuaghan@users.noreply.github.com>
2016-04-27 00:38:36 +1000

    Update site.pp with MOTD Changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,12 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 }
+
+# Change MOTD
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Puppet is Confusing!!!!\n",
+}
