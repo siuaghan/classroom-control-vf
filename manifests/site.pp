96a478d5a9cf3bab88c125413e11f64aab55eca2
siuaghan <siuaghan@users.noreply.github.com>
2016-04-27 19:17:06 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,9 +44,10 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  exec { "cowsay 'Welcome to Siuaghan' > /etc/motd"
+  
+  exec { "cowsay 'Welcome to Siuaghan' > /etc/motd" }
     path    =>'/usr/bin:/usr/bin/local',
     creates =>'/etc/motd',
-  }
+  
   
 }
