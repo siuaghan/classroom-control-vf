4a34d6112931788aa8b55bb26e0e4bf7b08ac712
siuaghan <siuaghan@users.noreply.github.com>
2016-04-28 20:39:08 +1000

    Update init.pp

--- nginx/manifests/init.pp
+++ nginx/manifests/init.pp
@@ -1,37 +1,37 @@
 class nginx {
-  file {
+  File {
     owner =>  'root',
     group =>  'root',
     mode  =>  '0664',
   }
   
-  package { 'nginx':
+  Package { 'nginx':
     ensure => present,
   }
   
   
-  file { ['/var/www', '/etc/nginx/conf.d']:
+  File { ['/var/www', '/etc/nginx/conf.d']:
     ensure => directory,
   }
   
-  file { '/var/www/index.html':
+  File { '/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
   
-  file { '/etc/nginx/nginx.conf':
+  File { '/etc/nginx/nginx.conf':
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
   
- file { '/etc/nginx/conf.d/default.conf':
+  File { '/etc/nginx/conf.d/default.conf':
     source  => 'puppet:///modules/nginx/default.conf',
     notify  => Service['nginx'],
     require => Package['nginx'],
   }
-  service { 'nginx':
+  Service { 'nginx':
     ensure    => running,
     enable    => true,
   }
