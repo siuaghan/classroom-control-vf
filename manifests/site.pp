81745bc85b59fc1c8f03a137bf0078c289460348
siuaghan <siuaghan@users.noreply.github.com>
2016-04-29 18:39:45 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+  # notify { "Hello, my name is ${::hostname}": }
   
   if $::$virtual != 'physical'  {
       $vmname = capitilize($::virtual)
