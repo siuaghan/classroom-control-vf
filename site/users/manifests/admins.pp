be03be9c9735306051bd593f987e6731e966f137
siuaghan <siuaghan@users.noreply.github.com>
2016-04-29 19:26:12 +1000

    Create admins.pp

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,13 @@
+class user::admins  {
+  users::managed_user {'joe':}
+  users::managed_user {'alice': 
+    group =>  staff},
+  }
+  users::managed_user {'aaron':
+    group =>  staff},
+  }  
+  group {'staff':
+    ensure  =>  present,
+  }
+}  
+  
