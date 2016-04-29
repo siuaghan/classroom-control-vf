c81cd091f849cc81d2807ff02a8f60347e5fcc6f
siuaghan <siuaghan@users.noreply.github.com>
2016-04-29 19:41:33 +1000

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,10 +1,10 @@
 class users::admins {
   users::managed_user {'joe':}
   users::managed_user {'alice': 
-    group => 'staff'},
+    group => 'staff',
   }
   users::managed_user {'aaron':
-    group => 'staff'},
+    group => 'staff',
   }  
   group {'staff':
     ensure  =>  present,
